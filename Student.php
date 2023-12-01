f15fc42dc01b7c2eabe8931d4cc6f811dee952a7
PedroTauzen <pedrotauzen@gmail.com>
2023-12-01 22:04:08 +0000

    Student.php Added variables: name, number, email, program and subjectList with (subjectName, grade and status)

--- Student.php
+++ Student.php
@@ -5,3 +5,12 @@
  * @date: 01/12/2023
  */
 
+$name = "Pedro Tauzen";
+$number = 23061;
+$email = "23061@stu.ipbeja.pt";
+$program = "Graduation";
+$subjectList = array("subjectName" => "Math", "grade" => 9.5, "status" => "Approved");
+
+
+
+
