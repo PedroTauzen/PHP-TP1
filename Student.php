6b8e6e15fa50c137c7d69c715c1124071fbb5dde
PedroTauzen <pedrotauzen@gmail.com>
2023-12-02 15:49:43 +0000

    Student.php Added set functions to set the attributes

--- Student.php
+++ Student.php
@@ -5,12 +5,42 @@
  * @date: 01/12/2023
  */
 
-$name = "Pedro Tauzen";
-$number = 23061;
-$email = "23061@stu.ipbeja.pt";
-$program = "Graduation";
-$subjectList = array("subjectName" => "Math", "grade" => 9.5, "status" => "Approved");
+class Student
+{
+    private string $name;
+    private int $number;
+    private string $email;
+    private string $program;
+    private array $subjectList;
 
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
 
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    }
 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function setProgram($program)
+    {
+        $this->program = $program;
+    }
+
+    public function setSubjectList($subjectName, $subjectGrade)
+    {
+        $subjectStatus = ($subjectGrade < 9.5) ? 'Disapproved' : 'Approved';
+        $this->subjectList[] = [
+            "subjectName" => $subjectName,
+            "subjectGrade" => $subjectGrade,
+            "subjectStatus" => $subjectStatus
+        ];
+    }
+}
 
